31ef6c89c395cd1b9b4e106e4263fb5c018f8e9b
musiur <musiur.opu@gmail.com>
2022-11-01 15:03:49 +0600

    footer added

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,153 @@
-import { Card } from "@nextui-org/react";
-import BackgroundImage from "../static/images/backgrounds/tailwindtop.png";
+import { Card, Input, Link, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import ButtonG from "../components/ButtonG";
 
 const Footer = () => {
+  const router = useRouter();
+  const footerContents = [
+    {
+      heading: "Company",
+      childrens: [
+        {
+          text: "About",
+          link: "",
+        },
+        {
+          text: "Blog",
+          link: "",
+        },
+        {
+          text: "Legal",
+          link: "",
+        },
+        {
+          text: "Partners",
+          link: "",
+        },
+        {
+          text: "Privacy",
+          link: "",
+        },
+      ],
+    },
+    {
+      heading: "Support",
+      childrens: [
+        {
+          text: "About",
+          link: "",
+        },
+        {
+          text: "Blog",
+          link: "",
+        },
+        {
+          text: "Legal",
+          link: "",
+        },
+        {
+          text: "Partners",
+          link: "",
+        },
+        {
+          text: "Privacy",
+          link: "",
+        },
+      ],
+    },
+    {
+      heading: "Solution",
+      childrens: [
+        {
+          text: "About",
+          link: "",
+        },
+        {
+          text: "Blog",
+          link: "",
+        },
+        {
+          text: "Legal",
+          link: "",
+        },
+        {
+          text: "Partners",
+          link: "",
+        },
+        {
+          text: "Privacy",
+          link: "",
+        },
+      ],
+    },
+    {
+      heading: "Legal",
+      childrens: [
+        {
+          text: "About",
+          link: "",
+        },
+        {
+          text: "Blog",
+          link: "",
+        },
+        {
+          text: "Legal",
+          link: "",
+        },
+        {
+          text: "Partners",
+          link: "",
+        },
+        {
+          text: "Privacy",
+          link: "",
+        },
+      ],
+    },
+  ];
   return (
-    <div className="">
-      <Card css={{ w: "100%", h: "200px", border: "0", borderRadius: "10px 10px 0 0" }}>
-        <Card.Body css={{ p: 0 }}>
-          <Card.Image
-            src={BackgroundImage.src}
-            width="100%"
-            height="100%"
-            objectFit="cover"
-            alt="Card example background"
-          />
-        </Card.Body>
-        <Card.Footer
-          isBlurred
-          css={{
-            position: "absolute",
-            bgBlur: "#ffffff00",
-            top: 0,
-            bottom:0,
-            zIndex: 1,
-            borderRadius: "10px 10px 0 0"
-          }}
-        >
-          <div className="containerG sectionG">
-            adfadfa
+    <div
+      className="containerG sectionG grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5 w-full"
+      style={{
+        background: "url(../static/images/backgrounds/tailwindtop.png)",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+      }}
+    >
+      {footerContents.map((item, i) => {
+        const { heading, childrens } = item;
+        return (
+          <div key={i}>
+            <h4 className="font-semibold text-xl lg:text-2xl">{heading}</h4>
+            <div>
+              {childrens.map((child, i) => {
+                const { text, link } = child;
+                return (
+                  <Text
+                    key={i}
+                    className="py-1 hover:text-blue-600 cursor-pointer"
+                    onClick={() => router.push(link ? link : "#")}
+                  >
+                    {text}
+                  </Text>
+                );
+              })}
+            </div>
           </div>
-        </Card.Footer>
-      </Card>
+        );
+      })}
+      <div className="w-full h-full">
+        <h4 className="font-semibold text-xl lg:text-2xl mb-5">
+          Subscribe to our newslater
+        </h4>
+        <p className="mb-5">
+          Eiusmod nulla anim labore amet mollit deserunt labore amet mollit
+          deserunt.
+        </p>
+        <Input className="mb-2 border border-gray-600" css={{width: "100%"}}/>
+        <ButtonG text="Subscribe" color="primary"/>
+      </div>
     </div>
   );
 };
